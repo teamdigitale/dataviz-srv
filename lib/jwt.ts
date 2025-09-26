86147c341d431c39cd315874b1387f24e6e191de
Lorezz <point.point@gmail.com>
2025-09-26 17:38:30 +0200

    feat:added infos to user

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,17 +1,18 @@
-import crypto from "crypto";
-import { sign, verify } from "jsonwebtoken";
-import type { User } from "@prisma/client";
-import type { CookieOptions, Response } from "express";
+import crypto from 'crypto';
+import { sign, verify } from 'jsonwebtoken';
+import type { User } from '@prisma/client';
+import type { CookieOptions, Response } from 'express';
 
-const JWT_SECRET = process?.env["JWT_SECRET"] || "";
+const JWT_SECRET = process?.env['JWT_SECRET'] || '';
 // const EXPIRE = 60; //seconds
-const EXPIRE = "7d";
+const EXPIRE = '1d';
 
 export interface IAccessTokenPayload {
-  userId: User["id"];
+  userId: User['id'];
+  name: User['email'];
 }
 export interface IRefreshTokenPayload {
-  userId: User["id"];
+  userId: User['id'];
   jti: string;
 }
 
@@ -20,8 +21,8 @@ export function generateAccessToken(
   payload: IAccessTokenPayload,
   expiresIn: string | number = EXPIRE
 ) {
-  console.log("Generating AccessToken", payload);
-  console.log("expiresIn", expiresIn);
+  console.log('Generating AccessToken', payload);
+  console.log('expiresIn', expiresIn);
 
   return sign(payload, JWT_SECRET, {
     expiresIn,
@@ -31,6 +32,7 @@ export function generateAccessToken(
 export function generateTokens(user: User) {
   const accessTokenPayload: IAccessTokenPayload = {
     userId: user.id,
+    name: user.email,
   };
   const accessToken = generateAccessToken(accessTokenPayload);
 
@@ -51,13 +53,13 @@ export function verifyAccessToken(token: string) {
   try {
     return verify(token, JWT_SECRET) as IAccessTokenPayload;
   } catch (error) {
-    console.log("Error verifyng AccessToken", error);
+    console.log('Error verifyng AccessToken', error);
     throw error;
   }
 }
 
 export function hashToken(token: string) {
-  return crypto.createHash("sha512").update(token).digest("hex");
+  return crypto.createHash('sha512').update(token).digest('hex');
 }
 
 // function sendRefreshToken(res: Response, token: string) {
@@ -70,7 +72,7 @@ export function hashToken(token: string) {
 
 export function setAccessTokenCookie(res: Response, token: string) {
   const expires = new Date(Date.now() + 60 * 60 * 1000);
-  const isProduction = process.env.NODE_ENV === "production";
+  const isProduction = process.env.NODE_ENV === 'production';
   const cookieOptions: CookieOptions = {
     expires,
     httpOnly: true,
@@ -79,10 +81,10 @@ export function setAccessTokenCookie(res: Response, token: string) {
     // domain: isProduction ? process.env.DOMAIN : "",
     secure: isProduction ? true : false,
   };
-  console.log("COOKIE OPTIONS", cookieOptions);
-  res.cookie("access_token", token, cookieOptions);
+  console.log('COOKIE OPTIONS', cookieOptions);
+  res.cookie('access_token', token, cookieOptions);
 }
 
 export function clearAccestokenCookie(res: Response) {
-  res.clearCookie("access_token");
+  res.clearCookie('access_token');
 }
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,62 +1,63 @@
-import { Router } from "express";
-import * as bcrypt from "bcrypt";
-import * as db from "../lib/db";
+import { Router } from 'express';
+import * as bcrypt from 'bcrypt';
+import * as db from '../lib/db';
 import {
   generateTokens,
   setAccessTokenCookie,
   clearAccestokenCookie,
-} from "../lib/jwt";
-import * as z from "zod";
-import { sendActivationEmail, sendResetPasswordEmail } from "../lib/email";
-import { requireUser, validateRequest } from "../lib/middlewares";
+} from '../lib/jwt';
+import * as z from 'zod';
+import { sendActivationEmail, sendResetPasswordEmail } from '../lib/email';
+import { requireUser, validateRequest } from '../lib/middlewares';
 
-const APP_URL = process.env.APP_URL || "/";
+const APP_URL = process.env.APP_URL || '/';
 
 const router = Router();
 
-router.get("/user", (req: any, res) => {
-  console.log("check user");
+router.get('/user', (req: any, res) => {
+  console.log('check user');
   try {
     const user = req?.user || null;
     if (!user) {
       return res.status(401).json(null);
     }
-    console.log("user found", user);
-    return res.status(201).json({ user });
+    console.log('user found', user);
+    const token = req?.token || null;
+    return res.status(201).json({ ...user, token });
   } catch (error) {
-    console.log("Auth user ERROR", error);
+    console.log('Auth user ERROR', error);
   }
 });
 
 const registerSchema = z.object({
   email: z
     .string({
-      required_error: "Email is required",
+      required_error: 'Email is required',
     })
-    .email("Invalid email or password"),
+    .email('Invalid email or password'),
   password: z
-    .string({ required_error: "Password is required" })
-    .min(7, "Password must be at least 7 characters long"),
+    .string({ required_error: 'Password is required' })
+    .min(7, 'Password must be at least 7 characters long'),
 });
 router.post(
-  "/register",
+  '/register',
   validateRequest({ body: registerSchema }),
   async (req, res, next) => {
     try {
       const { email, password } = req.body;
       if (!email || !password) {
         res.status(400);
-        throw new Error("You must provide an email and a password.");
+        throw new Error('You must provide an email and a password.');
       }
       const existingUser = await db.findUserByEmail(email);
       if (existingUser) {
         res.status(409);
-        return res.json({ error: { message: "Email already in use." } });
+        return res.json({ error: { message: 'Email already in use.' } });
       }
       const user = await db.createUserByEmailAndPassword({ email, password });
       //@TODO SEND EMAIL TO ACTIVATE USER
       const pin = await db.createCode(user.id);
-      console.log("pin", pin);
+      console.log('pin', pin);
       await sendActivationEmail(user, pin);
 
       // const { accessToken } = generateTokens(user);
@@ -72,23 +73,23 @@ router.post(
 const loginSchema = z.object({
   email: z
     .string({
-      required_error: "Email is required",
+      required_error: 'Email is required',
     })
-    .email("Invalid email or password"),
-  password: z.string({ required_error: "Password is required" }),
+    .email('Invalid email or password'),
+  password: z.string({ required_error: 'Password is required' }),
 });
 
 router.post(
-  "/login",
+  '/login',
   validateRequest({ body: loginSchema }),
   async (req, res, next) => {
-    console.log("login");
+    console.log('login');
     try {
       const { email, password } = req.body;
       const existingUser = await db.findUserByEmail(email);
       if (!existingUser) {
         res.status(401);
-        throw new Error("Invalid login credentials.");
+        throw new Error('Invalid login credentials.');
       }
       const validPassword = await bcrypt.compare(
         password,
@@ -96,7 +97,7 @@ router.post(
       );
       if (!validPassword) {
         res.status(401);
-        throw new Error("Invalid login credentials.");
+        throw new Error('Invalid login credentials.');
       }
       const { accessToken } = generateTokens(existingUser);
       setAccessTokenCookie(res, accessToken);
@@ -113,75 +114,75 @@ router.post(
 const recoverSchema = z.object({
   email: z
     .string({
-      required_error: "Email is required",
+      required_error: 'Email is required',
     })
-    .email("Invalid email or password"),
+    .email('Invalid email or password'),
 });
 
 router.post(
-  "/recover",
+  '/recover',
   validateRequest({ body: recoverSchema }),
   async (req: any, res) => {
     clearAccestokenCookie(res);
     const { email } = req.body;
-    console.log("recover", email);
+    console.log('recover', email);
     const user = await db.findUserByEmail(email);
     if (user) {
-      console.log("user", user);
+      console.log('user', user);
       const pin = await db.createCode(user.id);
-      console.log("pin", pin);
+      console.log('pin', pin);
       await sendResetPasswordEmail(user, pin);
     }
     return res.status(200);
   }
 );
 
-router.get("/logout", (req: any, res) => {
-  console.log("logout, bye");
+router.get('/logout', (req: any, res) => {
+  console.log('logout, bye');
   clearAccestokenCookie(res);
   return res.status(204);
 });
 
 const verifySchema = z.object({
   uid: z.string({
-    required_error: "uid is required",
+    required_error: 'uid is required',
   }),
-  code: z.string({ required_error: "code is required" }),
+  code: z.string({ required_error: 'code is required' }),
 });
 router.post(
-  "/verify",
+  '/verify',
   validateRequest({ body: verifySchema }),
   async (req: any, res) => {
     const { uid, code } = req.body;
-    console.log("uid:", uid, ", code:", code);
+    console.log('uid:', uid, ', code:', code);
     if (!uid || !code) return res.status(401);
     if (req.user) {
-      console.log("req.user?", req.user);
+      console.log('req.user?', req.user);
       if (req.user.id !== uid) {
-        return res.status(400).json({ error: "Invalid user activation." });
+        return res.status(400).json({ error: 'Invalid user activation.' });
       }
     }
     const user = await db.findUserById(uid);
-    console.log("user?", user);
+    console.log('user?', user);
     if (!user) {
-      return res.status(400).json({ error: "User not found." });
+      return res.status(400).json({ error: 'User not found.' });
     }
     const pin = await db.findCodeByUid(uid);
-    console.log("pin?", pin);
+    console.log('pin?', pin);
     if (!pin) {
-      console.log("pin not found");
-      return res.status(400).json({ error: "Code invalid or expired." });
+      console.log('pin not found');
+      return res.status(400).json({ error: 'Code invalid or expired.' });
     }
     if (`${pin}`.trim() !== `${code}`.trim()) {
-      console.log("pin are not equals");
-      return res.status(400).json({ error: "Code invalid or expired." });
+      console.log('pin are not equals');
+      return res.status(400).json({ error: 'Code invalid or expired.' });
     }
-    console.log("user verifyed");
+    console.log('user verifyed');
     const userValue = await db.setVerifyed(user.id);
 
     await db.destroyCodes(user.id);
 
-    console.log("login user");
+    console.log('login user');
     const { accessToken } = generateTokens(userValue);
     setAccessTokenCookie(res, accessToken);
     return res.json({ auth: true });
@@ -190,38 +191,38 @@ router.post(
 
 const confirmSchema = z.object({
   uid: z.string({
-    required_error: "uid is required",
+    required_error: 'uid is required',
   }),
-  code: z.string({ required_error: "code is required" }),
+  code: z.string({ required_error: 'code is required' }),
 });
 router.get(
-  "/confirm/:uid/:code",
+  '/confirm/:uid/:code',
   validateRequest({ params: confirmSchema }),
   async (req: any, res) => {
     const { uid, code } = req.params;
-    console.log("uid:", uid, ", code:", code);
+    console.log('uid:', uid, ', code:', code);
     if (!uid || !code) return res.status(401);
     if (req.user) {
-      console.log("req.user?", req.user);
+      console.log('req.user?', req.user);
       if (req.user.userId !== uid) {
-        return res.status(400).json({ error: "Invalid user activation." });
+        return res.status(400).json({ error: 'Invalid user activation.' });
       }
     }
     const user = await db.findUserById(uid);
-    console.log("user?", user);
+    console.log('user?', user);
     if (!user) {
-      return res.status(400).json({ error: "User not found." });
+      return res.status(400).json({ error: 'User not found.' });
     }
     const pin = await db.findCodeByUid(uid);
-    console.log("pin?", pin);
+    console.log('pin?', pin);
     if (!pin || pin !== code) {
-      return res.status(400).json({ error: "Code invalid or expired." });
+      return res.status(400).json({ error: 'Code invalid or expired.' });
     }
     const userValue = await db.setVerifyed(user.id);
     await db.destroyCodes(user.id);
     const { accessToken } = generateTokens(userValue);
     setAccessTokenCookie(res, accessToken);
-    console.log("validate code end");
+    console.log('validate code end');
     // return res.json({ auth: true });
     return res.redirect(APP_URL);
   }
@@ -230,21 +231,21 @@ router.get(
 const changePwdSchema = z.object({
   email: z
     .string({
-      required_error: "Email is required",
+      required_error: 'Email is required',
     })
-    .email("Invalid email or password"),
+    .email('Invalid email or password'),
   password: z
-    .string({ required_error: "Password is required" })
-    .min(7, "Password must be at least 7 characters long"),
+    .string({ required_error: 'Password is required' })
+    .min(7, 'Password must be at least 7 characters long'),
 });
 router.put(
-  "/pwd",
+  '/pwd',
   [validateRequest({ body: changePwdSchema }), requireUser],
   async (req: any, res: any) => {
     const user = req.user;
     const { password } = req.body;
     if (!user || !password) {
-      return res.status(400).json({ error: "User and password are required." });
+      return res.status(400).json({ error: 'User and password are required.' });
     }
     await db.changePassword(user.id, password);
     return res.status(204);
