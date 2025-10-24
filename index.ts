6781a7dd259041d47815278f482d149a329c4ebb
Lorezz <point.point@gmail.com>
2025-10-24 17:10:04 +0200

    wip integrationg swagger

--- index.ts
+++ index.ts
@@ -7,11 +7,14 @@ import authRouter from './routes/auth.ts';
 import chartRouter from './routes/charts.ts';
 import dashRouter from './routes/dashboards.ts';
 import suggestionsRouter from './routes/hints.ts';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 // import seedUsers from "./seeds/seed-users.ts";
 // import * as db from "./lib/db.ts";
 
-const whitelist = process?.env.DOMAINS?.split(',') || 'http://localhost:3000';
-
+const HOST = process?.env.HOST || 'http://localhost';
+const PORT = process?.env.PORT || 3003;
+const whitelist = process?.env.DOMAINS?.split(',') || `${HOST}:${PORT}`;
 const app = express();
 
 // app.use(helmet());
@@ -25,16 +28,53 @@ const app = express();
 // );
 app.use(
   cors({
-    origin: ['localhost', 'http://localhost:3000'],
+    origin: ['localhost', HOST, `${HOST}:${PORT}`],
     credentials: true,
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   })
 );
 // app.use(express.bodyParser({ limit: "2mb" }));
+
+const options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Dataviz API',
+      version: '0.1.0',
+      description: 'Api to create and publish simple charts and their data',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'LogRocket',
+        url: 'https://dataviz.developers.it',
+        email: 'contacts@developers.it',
+      },
+    },
+    servers: [
+      {
+        url: `${HOST}:${PORT}`,
+      },
+    ],
+  },
+  apis: ['./routes/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    explorer: true,
+    customCssUrl:
+      'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css',
+  })
+);
+
 app.use(cookieParser());
 app.use(middlewares.checkAuth);
 app.use(express.json({ limit: '500kb' }));
-const port = 3003;
 
 app.get('/', (req, res) => {
   res.json('^^');
@@ -54,8 +94,8 @@ app.use('/api/hints', suggestionsRouter as Router);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
 
 export default app;
