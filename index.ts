67ef448c3de7d12a60eeeceeba1412a617806154
Lorezz <point.point@gmail.com>
2025-10-28 16:32:04 +0100

    refactor: setup env variables for deploy with default

--- index.ts
+++ index.ts
@@ -1,55 +1,51 @@
-import express, { Router } from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import * as middlewares from './lib/middlewares.ts';
-import authRouter from './routes/auth.ts';
-import chartRouter from './routes/charts.ts';
-import dashRouter from './routes/dashboards.ts';
-import suggestionsRouter from './routes/hints.ts';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import express, { Router } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import * as middlewares from "./lib/middlewares.ts";
+import authRouter from "./routes/auth.ts";
+import chartRouter from "./routes/charts.ts";
+import dashRouter from "./routes/dashboards.ts";
+import suggestionsRouter from "./routes/hints.ts";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 // import seedUsers from "./seeds/seed-users.ts";
-// import * as db from "./lib/db.ts";
 
-const HOST = process?.env.HOST || 'http://localhost';
-const PORT = process?.env.PORT || 3003;
-const whitelist = process?.env.DOMAINS?.split(',') || `${HOST}:${PORT}`;
+const HOST = process.env.HOST || "http://localhost";
+const PORT = process.env.PORT || 3003;
+const whitelist = process.env.DOMAINS?.split(",") || [
+  "localhost",
+  HOST,
+  `${HOST}:${PORT}`,
+];
+const UPLOAD_SIZE_LIMIT = process.env.UPLOAD_SIZE_LIMIT || "15mb";
+const ROUTES_PREFIX = process.env.ROUTES_PREFIX || "/api";
 const app = express();
 
-// app.use(helmet());
-// app.use(
-//   cors({
-//     origin: whitelist,
-//     // credentials: true,
-//     // origin: whitelist,
-//     // methods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-//   })
-// );
+app.use(helmet());
 app.use(
   cors({
-    origin: ['localhost', HOST, `${HOST}:${PORT}`],
+    origin: whitelist,
     credentials: true,
-    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    methods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
-// app.use(express.bodyParser({ limit: "2mb" }));
 
 const options = {
   definition: {
-    openapi: '3.1.0',
+    openapi: "3.1.0",
     info: {
-      title: 'Dataviz API',
-      version: '0.1.0',
-      description: 'Api to create and publish simple charts and their data',
+      title: "Dataviz API",
+      version: "0.1.0",
+      description: "Api to create and publish simple charts and their data",
       license: {
-        name: 'MIT',
-        url: 'https://spdx.org/licenses/MIT.html',
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
       },
       contact: {
-        name: 'LogRocket',
-        url: 'https://dataviz.developers.it',
-        email: 'contacts@developers.it',
+        name: "Developers Staff",
+        url: "https://dataviz.innovazione.gov.it",
+        email: "contacts@developers.it",
       },
     },
     servers: [
@@ -58,24 +54,28 @@ const options = {
       },
     ],
   },
-  apis: ['./routes/*.ts'],
+  apis: ["./routes/*.ts"],
 };
 
 const specs = swaggerJsdoc(options);
+
+app.use(cookieParser());
+app.use(middlewares.checkAuth);
+app.use(express.json({ limit: UPLOAD_SIZE_LIMIT }));
+app.use(`${ROUTES_PREFIX}/auth`, authRouter as Router);
+app.use(`${ROUTES_PREFIX}/charts`, chartRouter as Router);
+app.use(`${ROUTES_PREFIX}/dashboards`, dashRouter as Router);
+app.use(`${ROUTES_PREFIX}/hints`, suggestionsRouter as Router);
 app.use(
-  '/api-docs',
+  `${ROUTES_PREFIX}/docs`,
   swaggerUi.serve,
   swaggerUi.setup(specs, {
     explorer: true,
     customCssUrl:
-      'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css',
+      "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
   })
 );
-
-app.use(cookieParser());
-app.use(middlewares.checkAuth);
-app.use(express.json({ limit: '500kb' }));
-
+/* old default route
 app.get('/', (req, res) => {
   res.json('^^');
 });
@@ -86,11 +86,6 @@ app.get("/seed", (req, res) => {
   res.json({ result });
 });
 */
-
-app.use('/api/auth', authRouter as Router);
-app.use('/api/charts', chartRouter as Router);
-app.use('/api/dashboards', dashRouter as Router);
-app.use('/api/hints', suggestionsRouter as Router);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
